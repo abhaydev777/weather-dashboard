2f537c1cc3f51dcde945f4f41d9dfbbddb7f795a
Abhay Pratap Singh <abhay701734@gmail.com>
2025-02-20 01:39:31 +0530

    Add environment variable configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const API_KEY = '0e98c18bd8d77ce1566c7fb4cd881f20';
+export const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 export const WEATHER_ICON_URL = 'https://openweathermap.org/img/wn'; 
